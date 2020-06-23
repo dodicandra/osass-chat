7662b3bdc05b11c25603be3284b0d6e14e7a1097
dodi candra <cekiddot@gmail.com>
2020-06-23 14:16:59 +0700

    after setup oneSignal and firebase message 📢 📱📳

--- App.tsx
+++ App.tsx
@@ -2,6 +2,8 @@ import {NavigationContainer} from '@react-navigation/native';
 import {Loading, Splash} from 'components';
 import React, {useEffect, useState} from 'react';
 import {Modal, StatusBar} from 'react-native';
+//@ts-expect-error
+import OneSignal from 'react-native-onesignal';
 import {Provider, useDispatch, useSelector} from 'react-redux';
 import {Auth, DrawerScreen} from 'router';
 import {RootState, setToken, store} from 'store';
@@ -41,6 +43,48 @@ const MainApp = () => {
 };
 
 const App = () => {
+  OneSignal.setLogLevel(6, 0);
+
+  OneSignal.init('2e4f9f18-4700-452a-8510-a1162015e160', {
+    kOSSettingsKeyAutoPrompt: false,
+    kOSSettingsKeyInAppLaunchURL: false,
+    kOSSettingsKeyInFocusDisplayOption: 2,
+  });
+  OneSignal.inFocusDisplaying(2);
+
+  const onReceived = (notification: any) => {
+    console.log('Notification received: ', notification);
+  };
+
+  const onOpened = (openResult: {
+    notification: {
+      payload: {body: any; additionalData: any};
+      isAppInFocus: any;
+    };
+  }) => {
+    console.log('Message: ', openResult.notification.payload.body);
+    console.log('Data: ', openResult.notification.payload.additionalData);
+    console.log('isActive: ', openResult.notification.isAppInFocus);
+    console.log('openResult: ', openResult);
+  };
+
+  const onIds = (device: any) => {
+    console.log('Device info: ', device);
+  };
+  const myiOSPromptCallback = (_permission: any) => {};
+
+  useEffect(() => {
+    OneSignal.promptForPushNotificationsWithUserResponse(myiOSPromptCallback);
+    OneSignal.addEventListener('received', onReceived);
+    OneSignal.addEventListener('opened', onOpened);
+    OneSignal.addEventListener('ids', onIds);
+    return () => {
+      OneSignal.removeEventListener('received', onReceived);
+      OneSignal.removeEventListener('opened', onOpened);
+      OneSignal.removeEventListener('ids', onIds);
+    };
+  }, []);
+
   return (
     <Provider store={store}>
       <StatusBar
